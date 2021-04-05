3c81099b2c8805d305bd858009e0868c7e2d6cc6
staszewski <kamilstaszewski95@gmail.com>
2021-04-05 18:38:09 +0200

    Lint fix

--- src/data-structures/queue/queue.test.ts
+++ src/data-structures/queue/queue.test.ts
@@ -1,27 +1,26 @@
-import Queue from "./queue";
+import Queue from './queue';
 
 test('correctly enqueues objects', () => {
-    const queue = new Queue()
-    queue.enqueue({"value": 1})
-    queue.enqueue({"value": 2})
-    const output = queue.queue
-    expect(output[0].value).toEqual(1)
-    expect(output[1].value).toEqual(2)
+  const queue = new Queue();
+  queue.enqueue({ value: 1 });
+  queue.enqueue({ value: 2 });
+  const output = queue.queue;
+  expect(output[0].value).toEqual(1);
+  expect(output[1].value).toEqual(2);
 });
 
 test('enqueues correctly objects', () => {
-    const queue = new Queue()
-    queue.enqueue({"value": 1})
-    queue.enqueue({"value": 2})
-    queue.dequeue()
-    expect(queue.queue[0]).toBeUndefined()
-    expect(queue.queue[1].value).toEqual(2)
-    queue.enqueue({"value": 3})
-    expect(queue.queue[2].value).toEqual(3)
+  const queue = new Queue();
+  queue.enqueue({ value: 1 });
+  queue.enqueue({ value: 2 });
+  queue.dequeue();
+  expect(queue.queue[0]).toBeUndefined();
+  expect(queue.queue[1].value).toEqual(2);
+  queue.enqueue({ value: 3 });
+  expect(queue.queue[2].value).toEqual(3);
 });
 
-
 test('return false when there is no items to dequeue', () => {
-    const queue = new Queue()
-    expect(queue.dequeue()).toBeFalsy()
+  const queue = new Queue();
+  expect(queue.dequeue()).toBeFalsy();
 });
